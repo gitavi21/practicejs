662919eeaf448ba7a096699a5a637895527b5330
gitavi21 <99716148+gitavi21@users.noreply.github.com>
2026-03-06 01:09:50 +0530

    Update debounce.js

--- debounce.js
+++ debounce.js
@@ -1,1 +1,15 @@
 
+function Debouncing(){
+  function change(e){
+    
+    debounce(()=>{console.log(e.target.value)},1000)()
+  }
+  function debounce(fn,delay){
+    let timer;
+    return function (...args){
+      clearTimeout(timer)
+      timer = setTimeout(()=>{
+        fn(...args)
+      },delay)
+    }
+  }
